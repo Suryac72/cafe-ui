6141e8aa2fb9a179542a3eff0600bb39fdb15864
surya <suryac72@gmail.com>
2024-01-09 22:05:11 +0530

    fix(build issues)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory
+    assetsDir: 'assets', // Specify the assets directory
+    sourcemap: true, // Enable source maps
+    minify: 'terser', // Minify using Terser
+  },
+});
